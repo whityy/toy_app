676313c3d77ca1d9a7584b3d6a6f1f13786d088c
Blanca Sanchez <blancarocio.sanchez@gmail.com>
2015-07-22 12:44:40 +0200

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def helloWorld
+  	render text: "Hola, mundo!"
+  end
+
+  def goodbye
+  	render text: "Goodbye, world!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  #root 'welcome#index'
+  get 'helloWorld' => 'application#helloWorld'
+  get 'goodbye' => 'application#goodbye'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
